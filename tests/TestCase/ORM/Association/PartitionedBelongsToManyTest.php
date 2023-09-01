6ad867412dc1db8a0fa0c9671b06bd27fb34a723
ndm2 <ndm2@users.noreply.github.com>
2023-09-01 04:00:23 +0200

    Skip tests that currently cause MariaDB to crash.

--- tests/TestCase/ORM/Association/PartitionedBelongsToManyTest.php
+++ tests/TestCase/ORM/Association/PartitionedBelongsToManyTest.php
@@ -861,6 +861,7 @@ class PartitionedBelongsToManyTest extends TestCase
     public function testTranslationsEavOnlyTranslated(string $loaderStrategy, string $filterStrategy): void
     {
         $this->skipInSubqueryCTEStrategyIfSqlServer($filterStrategy);
+        $this->skipInSubqueryCTEAndTableStrategyIfMariaDB11($filterStrategy);
 
         I18n::setLocale('de_DE');
 
@@ -949,6 +950,7 @@ class PartitionedBelongsToManyTest extends TestCase
     public function testTranslationsShadowTableOnlyTranslated(string $loaderStrategy, string $filterStrategy): void
     {
         $this->skipInSubqueryCTEStrategyIfSqlServer($filterStrategy);
+        $this->skipInSubqueryCTEAndTableStrategyIfMariaDB11($filterStrategy);
 
         I18n::setLocale('de_DE');
 
